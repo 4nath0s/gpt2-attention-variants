3c8b2e988c487e4bf2119dd0509975a6faa05a4e
Antoine <ant.danaels@gmail.com>
2026-07-02 22:21:11 +0200

    setting up the basic files

---
+++ config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class GPTConfig:
+    vocab_size: int = 50257
+    context_length: int = 1024
+    d_model: int = 768
+    n_layers: int = 12
+    n_heads: int = 12
+    dropout: float = 0.1
+    attention: str = "mha" 
+
