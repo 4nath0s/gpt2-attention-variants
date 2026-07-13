52daa15c367574c221dca8bb9a84fbf3347a44ba
Antoine <ant.danaels@gmail.com>
2026-07-13 17:21:33 +0200

    wip: KV cache implementation for MHA

--- model.py
+++ model.py
@@ -55,11 +55,11 @@ class TransformerBlock(nn.Module):
         self.norm1 = LayerNorm(cfg.d_model)
         self.norm2 = LayerNorm(cfg.d_model)
         self.drop_shortcut = nn.Dropout(cfg.dropout)
-    def forward(self, x):
-
+        
+    def forward(self, x, use_cache=False):
         shortcut = x
         x = self.norm1(x)
-        x = self.att(x)
+        x = self.att(x, use_cache=use_cache)
         x = self.drop_shortcut(x)
         x = x + shortcut
         shortcut = x
@@ -77,20 +77,31 @@ class GPTModel(nn.Module):
         self.pos_emb = nn.Embedding(cfg.context_length, cfg.d_model)
         self.drop_emb = nn.Dropout(cfg.dropout)
 
-        self.trf_blocks = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg.n_layers)])
+        self.trf_blocks = nn.ModuleList([TransformerBlock(cfg) for _ in range(cfg.n_layers)])
 
         self.final_norm = LayerNorm(cfg.d_model)
         self.out_head = nn.Linear(cfg.d_model, cfg.vocab_size, bias=False)
-    def forward(self, in_idx):
+        self.current_pos = 0
+        
+    def forward(self, in_idx, use_cache=False):
         batch_size, seq_len = in_idx.shape
-        tok_embeds = self.tok_emb(in_idx)
 
-        pos_embeds = self.pos_emb(
-        torch.arange(seq_len, device=in_idx.device)
-        )
-        x = tok_embeds + pos_embeds
+        if use_cache:
+            start = self.current_pos
+            self.current_pos += seq_len
+        else:
+            start = 0
+        pos = torch.arange(start, start + seq_len, device=in_idx.device)
+
+        x = self.tok_emb(in_idx) + self.pos_emb(pos)
         x = self.drop_emb(x)
-        x = self.trf_blocks(x)
+        for blk in self.trf_blocks:
+            x = blk(x, use_cache=use_cache)
         x = self.final_norm(x)
-        logits = self.out_head(x)
-        return logits
+        return self.out_head(x)
+    
+    
+    def reset_kv_cache(self):
+        for blk in self.trf_blocks:
+            blk.att.reset_cache()
+        self.current_pos = 0
